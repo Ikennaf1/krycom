c266390df8ddc3291c4aaa60336db345a8ab395b
Ikennaf1 <iiikbros202@gmail.com>
2023-09-11 16:57:17 +0100

    Nav in progress

--- src/app/Nav.js
+++ src/app/Nav.js
@@ -1,18 +1,33 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 
 const Nav = () => {
-    return (
-        <nav className="fixed w-full">
-            <div className="body-margin flex justify-between items-center h-16 border-b border-gray-700">
-                <div className="text-lg font-bold">
-                    <Link href="/"><h1>Krycom</h1></Link>
-                </div>
 
-                <div>
-                    {/*  */}
+    const [ showNav, setShowNav ] = useState( false );
+
+    const handleNav = () => {
+        setShowNav((showNav) => !showNav);
+    }
+
+    return (
+        <>
+            <nav className="fixed w-full">
+                <div className="body-margin flex justify-between items-center h-16 border-b border-gray-700">
+                    <div className="text-lg font-bold">
+                        <Link href="/"><h1>Krycom</h1></Link>
+                    </div>
+
+                    <div>
+                        {/*  */}
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+
+            <button className="pt-64" type="button" onClick={ () => handleNav() }>this btn</button>
+
+            {/* <div className={`fixed w-full h-full bg-black/80 z-40`}></div> */}
+        </>
     )
 }
 
